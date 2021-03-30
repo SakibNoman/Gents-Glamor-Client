b7deb214e95e3921aa6c2f1f25b497cf63ee369b
Sakib Noman <skbnmn@gmail.com>
2021-03-31 05:04:47 +0600

    home design completed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import allProduct from '../../fakeData/fakeData.json';
+import SingleProduct from '../SingleProduct/SingleProduct';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is homepage</h1>
+        <div className="d-flex justify-content-center" >
+            <div className="row d-flex justify-content-center" >
+                {
+                    allProduct.map(each => <SingleProduct item={each} ></SingleProduct>)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/SingleProduct/SingleProduct.js
+++ src/Components/SingleProduct/SingleProduct.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import imageUrl from '../../images/Atar.jpg';
+import './SingleProduct.css';
 
-const SingleProduct = () => {
+const SingleProduct = (props) => {
+    const { product_name, price } = props.item;
+
     return (
-        <div>
-            <h4>This is product</h4>
+        <div className="col-md-3 d-flex flex-column single-card">
+            <div className="card-image flex-grow-5 text-center" >
+                <img src={imageUrl} alt="" />
+            </div>
+            <div className="flex-grow-1 d-flex flex-column justify-content-end">
+                <div className="mb-2" ><h4>{product_name}</h4></div>
+                <div className="flex-grow-2 d-flex justify-content-between" >
+                    <div><h2>{price}</h2></div>
+                    <div> <button className="btn btn-dark">Buy Now</button> </div>
+                </div>
+            </div>
         </div>
     );
 };
